153a0af86a31590d86cb3134fb583cade8d63bf5
Volkan <volkankanat.mdbf17@iste.edu.tr>
2024-04-25 05:15:46 +0300

    feat : styles add

---
+++ styles/styles.ts
@@ -0,0 +1,86 @@
+const styles = {
+    // Size
+    huge: {
+      width: 1000,
+      height: 1000,
+      padding: 32,
+      margin: 32,
+    },
+    xLarge: {
+      width: 20,
+      height: 20,
+      padding: 24,
+      margin: 20,
+    },
+    large: {
+      width: 12,
+      height: 12,
+      padding: 20,
+      margin: 16,
+    },
+    medium: {
+      width: 8,
+      height: 8,
+      padding: 16,
+      margin: 12,
+    },
+    small: {
+      width: 4,
+      height: 4,
+      padding: 8,
+      margin: 8,
+    },
+  
+    // Typography
+    hugeText: {
+      fontSize: 32,
+    },
+    xLargeText: {
+      fontSize: 22,
+    },
+    largeText: {
+      fontSize: 18,
+    },
+    mediumText: {
+      fontSize: 16,
+    },
+    normalText: {
+      fontSize: 14,
+    },
+    smallText: {
+      fontSize: 12,
+    },
+    xSmallText: {
+      fontSize: 10,
+    },
+  
+    // Colors
+    text: {
+      primary: '#374151',
+      secondary: '#687280',
+      tertiary: '#9CA3AF',
+      disabled: '#687280',
+      placeholder: '#687280',
+      white: '#FFFFFF',
+    },
+    background: {
+      primary: '#F1F5F9',
+      secondary: '#F5F5F5',
+    },
+    foreground: {
+      primary: '#111827',
+      secondary: '#374151',
+      tertiary: '#485563',
+      quaternary: '#687280',
+      quinary: '#9CA3AF',
+      disabled: '#9CA3AF',
+    },
+    border: {
+      primary: '#D1D5DB',
+      secondary: '#E5E7EB',
+      active: '#374151',
+      disabled: '#E5E7EB',
+    },
+  };
+  
+  export default styles;
