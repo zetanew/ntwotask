130a1818bd4dcdfecc444f348eb9f3f3f0e5dfaf
Volkan <volkankanat.mdbf17@iste.edu.tr>
2024-04-25 09:42:45 +0300

    fix : index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import { Text, View } from '@/components/Themed';
-
-export default function index() {
+const index = () => {
   return (
-    <View>
-      <Text>From index</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hosgeldiniz</Text>
+      
     </View>
   );
-}
+};
 
+export default index;
